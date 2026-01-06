fa730482ce792a072fe1d940c3d6e1cf734538c9
neoxush <neoxu.sh@hotmail.com>
2026-01-06 10:21:32 +0800

    Revert "Fix Depandabots ticket #42 #43"
    This reverts commit acb5b9c199756aa4f96a756a313c0b3affc7f928.

--- games/typingfeast/vite.config.ts
+++ games/typingfeast/vite.config.ts
@@ -1,29 +1,12 @@
 import { defineConfig } from 'vite';
-import { fileURLToPath, URL } from 'node:url';
 
 export default defineConfig({
   server: {
     port: 3000,
-    open: true,
-    fs: {
-      strict: true,
-      allow: [
-        // Search for workspace root
-        fileURLToPath(new URL('../../..', import.meta.url)),
-        // Your custom rules
-        '/src/',
-        '/public/',
-      ]
-    }
+    open: true
   },
   build: {
     outDir: 'dist',
-    sourcemap: true,
-    rollupOptions: {
-      input: {
-        main: fileURLToPath(new URL('./index.html', import.meta.url)),
-      },
-    },
-  },
-  appType: 'spa',
+    sourcemap: true
+  }
 });
