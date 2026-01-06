acb5b9c199756aa4f96a756a313c0b3affc7f928
neoxush <neoxu.sh@hotmail.com>
2026-01-06 10:19:16 +0800

    Fix Depandabots ticket #42 #43

--- games/typingfeast/vite.config.ts
+++ games/typingfeast/vite.config.ts
@@ -1,12 +1,29 @@
 import { defineConfig } from 'vite';
+import { fileURLToPath, URL } from 'node:url';
 
 export default defineConfig({
   server: {
     port: 3000,
-    open: true
+    open: true,
+    fs: {
+      strict: true,
+      allow: [
+        // Search for workspace root
+        fileURLToPath(new URL('../../..', import.meta.url)),
+        // Your custom rules
+        '/src/',
+        '/public/',
+      ]
+    }
   },
   build: {
     outDir: 'dist',
-    sourcemap: true
-  }
+    sourcemap: true,
+    rollupOptions: {
+      input: {
+        main: fileURLToPath(new URL('./index.html', import.meta.url)),
+      },
+    },
+  },
+  appType: 'spa',
 });
